92cce32be9c785d3b15606a64ec5cea4b9c59483
Souheab <souheab@protonmail.com>
2024-03-24 02:43:01 -0400

    Make seperate file for event handling functions

--- app.go
+++ app.go
@@ -3,10 +3,7 @@ package main
 import (
 	"os"
 	"path/filepath"
-	"strconv"
 	"time"
-
-	"github.com/gdamore/tcell/v2"
 )
 
 const (
@@ -28,75 +25,13 @@ func RunApp() {
 
 	s.Init()
 
-	go b.ProcessEventWorker()
+	go b.ProcessEventsWorker()
 
 	for {
 		b.Draw()
 		b.HandleKeyEvent()
 		time.Sleep(TickTime)
 	}
-}
-
-func (b *AppBackend) ProcessEventWorker() {
-	ch := b.InputChan
-
-	for {
-		ev := b.Screen.PollEvent()
-
-		switch ev := ev.(type) {
-		case *tcell.EventKey:
-			select {
-			case ch <- ev:
-			default:
-			}
-		}
-	}
-}
-
-func (b *AppBackend) HandleKeyEvent() {
-	ch := b.InputChan
-	select {
-	case ev := <-ch:
-		t := b.ActiveTab
-		switch ev.Key() {
-		case tcell.KeyCtrlC, tcell.KeyCtrlD:
-			b.ExitApp()
-		case tcell.KeyEsc:
-			t.InputCount = ""
-			return
-		}
-
-		inputKeyRune := ev.Rune()
-		if inputKeyRune >= '0' && inputKeyRune <= '9' {
-			t.AddToInputCount(inputKeyRune)
-			return
-		}
-
-		inputTimes := 1
-		if t.InputCount != "" {
-			inputTimes, _ = strconv.Atoi(t.InputCount)
-			t.ClearInputCount()
-		}
-
-		switch inputKeyRune {
-		case 'h', 'H':
-			t.Select(1, 0, DirectionLeft)
-		case 'j', 'J':
-			t.Select(inputTimes, 0, DirectionDown)
-		case 'k', 'K':
-			t.Select(inputTimes, 0, DirectionUp)
-		case 'l', 'L':
-			t.Select(1, 0, DirectionRight)
-		case '.':
-			t.ToggleDotfilesVisibility()
-		case 'q', 'Q':
-			b.ExitApp()
-		}
-		t.RunListChangedFunc()
-	default:
-		return
-	}
-
 }
 
 func (b *AppBackend) ExitApp() {
---
+++ events.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"strconv"
+
+	"github.com/gdamore/tcell/v2"
+)
+
+func (b *AppBackend) ProcessEventsWorker() {
+	ch := b.InputChan
+
+	for {
+		ev := b.Screen.PollEvent()
+
+		switch ev := ev.(type) {
+		case *tcell.EventKey:
+			select {
+			case ch <- ev:
+			default:
+			}
+		}
+	}
+}
+
+func (b *AppBackend) HandleKeyEvent() {
+	ch := b.InputChan
+	select {
+	case ev := <-ch:
+		t := b.ActiveTab
+		switch ev.Key() {
+		case tcell.KeyCtrlC, tcell.KeyCtrlD:
+			b.ExitApp()
+		case tcell.KeyEsc:
+			t.InputCount = ""
+			return
+		}
+
+		inputKeyRune := ev.Rune()
+		if inputKeyRune >= '0' && inputKeyRune <= '9' {
+			t.AddToInputCount(inputKeyRune)
+			return
+		}
+
+		inputTimes := 1
+		if t.InputCount != "" {
+			inputTimes, _ = strconv.Atoi(t.InputCount)
+			t.ClearInputCount()
+		}
+
+		switch inputKeyRune {
+		case 'h', 'H':
+			t.Select(1, 0, DirectionLeft)
+		case 'j', 'J':
+			t.Select(inputTimes, 0, DirectionDown)
+		case 'k', 'K':
+			t.Select(inputTimes, 0, DirectionUp)
+		case 'l', 'L':
+			t.Select(1, 0, DirectionRight)
+		case '.':
+			t.ToggleDotfilesVisibility()
+		case 'q', 'Q':
+			b.ExitApp()
+		}
+		t.RunListChangedFunc()
+	default:
+		return
+	}
+
+}
